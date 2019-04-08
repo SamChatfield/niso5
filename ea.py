b4449df3e88c4f00ff042ffa39beee818a3699fe
Sam Chatfield <samchatfield97@gmail.com>
2019-04-08 19:45:52 +0100

    Change generations to simulate and sort at the end so that the best solution at the end of the generation is known.

--- ea.py
+++ ea.py
@@ -12,6 +12,7 @@ import selection
 
 class EA:
     def __init__(self, lambda_, h, weeks,
+                 parent_proportion=0.25,
                  mutation_chance=0.5, mutation_rate=None):
         logging.debug('Create EA object with lambda=%s, h=%s, weeks=%s', lambda_, h, weeks)
         self._lambda = lambda_
@@ -22,7 +23,7 @@ class EA:
         # Default mutation rate of 1 / num_genes (where num_genes = 3h)
         self._mutation_rate = 1 / (3 * h) if mutation_rate is None else mutation_rate
 
-        self._num_parents = max(2, math.floor(lambda_ * 0.25))
+        self._num_parents = max(2, math.floor(lambda_ * parent_proportion))
 
         # Initialise population
         self._population = init.initialise(lambda_, h)
@@ -30,32 +31,40 @@ class EA:
     def run(self, generations):
         logging.debug('Running for %s generations', generations)
 
-        for gen in range(generations):
+        # Compute initial payoffs (fitness evaluation)
+        gen = 0
+        logging.debug('GEN %s', gen)
+        (payoffs, mean_attendance) = population.simulate(gen, self._population, self._weeks)
+        logging.info('Payoffs for gen %s:\n%s', gen, payoffs)
+        logging.info('Attendance for gen %s:\n%.4f\n', gen, (mean_attendance / self._lambda))
+
+        # Sort the initial population by payoffs
+        self._population = population.sorted_by_payoffs(self._population, payoffs)
+
+        for gen in range(1, generations):
             logging.debug('GEN %s', gen)
-
-            # Compute payoffs (fitness evaluation)
-            (payoffs, mean_attendance) = population.simulate(gen, self._population, self._weeks)
-            logging.info('Payoffs for gen %s:\n%s', gen, payoffs)
-            logging.info('Attendance for gen %s:\n%.4f\n', gen, (mean_attendance / self._lambda))
-
-            # Sort the population by payoffs
-            sorted_pop = population.sorted_by_payoffs(self._population, payoffs)
+            new_pop = self._population.copy()
 
             # Select parents
-            parents = selection.truncation(self._num_parents, sorted_pop)
+            parents = selection.truncation(self._num_parents, new_pop)
 
             # Generate children by crossover
             children = crossover.crossover(parents)
-
-            logging.debug('POP BEFORE:\n%s\n', sorted_pop)
-            sorted_pop[-children.size:] = children
-            logging.debug('POP AFTER:\n%s\n', sorted_pop)
+            # Replace worst solutions in the population with children
+            new_pop[-children.size:] = children
 
             # Mutation considering all individuals except the best
-            logging.debug('POP BEFORE:\n%s\n', sorted_pop)
-            sorted_pop[1:] = mutation.mutation(sorted_pop[1:], self._mutation_chance, self._mutation_rate)
-            logging.debug('POP AFTER:\n%s\n', sorted_pop)
+            new_pop[1:] = mutation.mutation(new_pop[1:], self._mutation_chance, self._mutation_rate)
+
+            # Compute payoffs of new population
+            (payoffs, mean_attendance) = population.simulate(gen, new_pop, self._weeks)
+            logging.info('Payoffs for gen %s:\n%s', gen, payoffs)
+            logging.info('Attendance for gen %s:\n%.4f\n', gen, (mean_attendance / self._lambda))
+
+            # Sort the new population by payoffs
+            sorted_pop = population.sorted_by_payoffs(new_pop, payoffs)
 
             self._population = sorted_pop
-
             assert self._population.size == self._lambda
+
+        return mean_attendance
